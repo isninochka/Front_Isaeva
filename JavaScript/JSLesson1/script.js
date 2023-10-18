6bedab508dd95cbdc1a5354eaf71b4cbce97f4a8
NinaIsaeva <ninaisaeva0919@gmail.com>
2023-10-18 16:52:15 +0200

    18.10.23

---
+++ JavaScript/JSLesson1/script.js
@@ -0,0 +1,31 @@
+/* alert("Hello")
+console.log(100) */
+// let name = "Nina"
+// name = "Nikita"
+// console.log(name)
+
+// const userName = "Nina"
+// console.log(userName)
+// const number = +prompt("Введите число")
+// console.log(number)
+// console.log(typeof number)
+// const username = "Nina"
+// const surname = "Isaeva"
+// console.log(username +"isaeva")
+// console.log(`Имя: ${username}, Фамилия: ${surname}`)
+// const username = prompt("Введите ваше имя")
+// console.log(`Привет, ${username}`)
+// const r = 100
+// const g = 57
+// const b = 141
+// console.log(`rgb (${r}, ${g}, ${b}) `)
+// const a = 30
+// const b = 20
+// console.log(a%b)
+// const bithYear = +prompt("Введите ваш год рождения")
+// console.log(`Вам, ${2023-bithYear} лет`)
+const degree = 20
+if(degree > 20){
+    console.log(`Можно идти в футболке`)
+} else {
+console.log(`Бррр... Надень куртку`)}
